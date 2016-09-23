b80f52e78b9c282db3bbd972d9124b6e11fcf862
peterAhn <xion486@gmail.com>
2016-09-23 22:47:46 +0900

    projectile damage dev

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -46,13 +46,24 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, U
 	SetRootComponent(ImpactBlast);
 	CollisionMesh->DestroyComponent();
 
+
+	UGameplayStatics::ApplyRadialDamage(
+		this,
+		ProjectileDamage,
+		GetActorLocation(),
+		ExplosionForce->Radius,	// for consistancy
+		UDamageType::StaticClass(),
+		TArray<AActor*>()	// damage all actor
+	);
+
+
 	FTimerHandle Timer;
 	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AProjectile::OnTimerExpire, DestroyDelay, false);
 }
 
 void AProjectile::OnTimerExpire()
 {
-	Destory();
+	Destroy();
 }
 
 void AProjectile::LaunchProjectile(float Speed)
